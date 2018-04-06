9f5249a3a2ab85dadd0bc96f2f124f74010b57fb
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-04-06 17:16:08 +0300

    Updated MSTest framework to 2.1, including adapter, added datarow tests

---
+++ TDD-CSharp/TDD-CSharp/Factorial/ParameterizedTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnderTests;
+
+namespace TDD_CSharp.Factorial
+{
+    [TestClass]
+    public class ParameterizedFactorialTests
+    {
+        [DataRow(0, 1)]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataTestMethod]
+        public void FactorialCalculations(int input, int expected)
+        {
+            Assert.AreEqual(expected, Factorials.Calculate(input));
+        }
+    }
+}
