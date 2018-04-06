0ce2150cbda1aa3a3ee4d4ac4e87de9e23748040
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-04-06 17:38:17 +0300

    Added file data driven test

--- TDD-CSharp/TDD-CSharp/Factorial/ParameterizedTests.cs
+++ TDD-CSharp/TDD-CSharp/Factorial/ParameterizedTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using UnderTests;
 
 namespace TDD_CSharp.Factorial
@@ -13,6 +14,21 @@ namespace TDD_CSharp.Factorial
         public void FactorialCalculations(int input, int expected)
         {
             Assert.AreEqual(expected, Factorials.Calculate(input));
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [DataTestMethod]
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV", 
+            @"Factorial\FactorialData.csv",
+            "FactorialData#csv", 
+            DataAccessMethod.Sequential)]
+        public void FactorialCalculationsFromFile()
+        {
+            int input = Convert.ToInt32(TestContext.DataRow[0]);
+            int result = Convert.ToInt32(TestContext.DataRow[1]);
+            Assert.AreEqual(result, Factorials.Calculate(input));
         }
     }
 }
